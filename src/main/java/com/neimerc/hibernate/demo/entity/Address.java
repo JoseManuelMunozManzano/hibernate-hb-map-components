12536ce135bd83475491d19c9dcf6bb43b9b8abb
Neimerc <Neil_Mercury@yahoo.es>
2020-05-13 20:40:20 +0200

    added embeddable pojo

---
+++ src/main/java/com/neimerc/hibernate/demo/entity/Address.java
@@ -0,0 +1,58 @@
+package com.neimerc.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+	@Column(name = "street")
+	private String street;
+	
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "zip")
+	private String zip;
+	
+	public Address() {
+		
+	}
+
+	public Address(String street, String city, String zip) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.zip = zip;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + ", zip=" + zip + "]";
+	}
+		
+}
